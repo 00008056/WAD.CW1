f89587cf60a023ef1a4c27af1d5aed12e924a8f9
00008056 <guzakbergenova@students.wiut.uz>
2021-03-20 16:06:31 +0300

    namespaces updated for views

--- RestaurantReviewWebApplication/RestaurantReviewWebApplication/Startup.cs
+++ RestaurantReviewWebApplication/RestaurantReviewWebApplication/Startup.cs
@@ -5,6 +5,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using RestaurantReviewWebApplication.DAL;
+using RestaurantReviewWebApplication.DAL.DBO;
+using RestaurantReviewWebApplication.DAL.Repos;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,8 +26,11 @@ namespace RestaurantReviewWebApplication
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Dependecy injection
+            services.AddScoped<IRepo<Author>, AuthorRepo>();
+            services.AddScoped<IRepo<Restaurant>, RestaurantRepo>();
+            services.AddScoped<IRepo<Review>, ReviewRepo>();
             services.AddControllersWithViews();
-
             services.AddDbContext<RestaurantReviewAppDbContext>(
                 options => options.UseSqlServer(Configuration.GetConnectionString("RestaurantReviewAppConnection")));
         }
